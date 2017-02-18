783df17bb9944826b1897fc2709beba09be36c7c
Marty Marshalek <mmarshalek@outlook.com>
2017-02-18 14:22:19 -0500

    Finsihed hw3c

--- hw3/hw3c.cpp
+++ hw3/hw3c.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
+#define _USE_MATH_DEFINES
 using namespace std;
-
-class polar {
-private:
-    double theta, dist
-public:
-    polar(double t = 0.0, double d = 0.0) : theta(t), dist(d) {}
-    vec2d convToVec(polar p) {
-
-    }
-}
 
 class vec2d{
 private:
@@ -19,14 +11,39 @@ public:
     vec2d(double x = 0.0, double y = 0.0) : X(x), Y(y) {}
 
     vec2d add(const vec2d v) const{
-        return Vec3d(X + v.X, Y + v.Y);
+        return vec2d(X + v.X, Y + v.Y);
     }
 
     friend ostream& operator <<(ostream& s, vec2d v) {
-        return s << "(" << v.X << ", " << v.Y << ")";
+        return s << "X = " << v.X << " Y = " << v.Y;
     }
 
     friend vec2d operator +(vec2d A, vec2d B) {
-        return Vec2d(A.X + B.X, A.Y + B.Y); 
+        return vec2d(A.X + B.X, A.Y + B.Y); 
     }
 };
+
+vec2d polarToVec(double theta, double dist){
+    return vec2d(dist*sin(theta*M_PI/180.0), dist*cos(theta*M_PI/180.0));
+}
+
+int main(){
+    vec2d robotLoc(0.0,0.0);
+    while(1){
+        double theta = 0.0;
+        double dist = 0.0;
+        cout << "Enter an angle (in degrees) and a distance (separated by a space)\n";
+        cin >> theta >> dist;
+
+        if (theta < 0 || dist < 0) {
+            break;
+        }
+        else
+            robotLoc = robotLoc + polarToVec(theta, dist);
+            // Using fixed and setprecision to round display to 4 decimal places
+            // Else cos(pi/2) doesn't return 0 and the results are weird
+            cout << fixed << setprecision(4) << "Location: " << robotLoc << "\n";
+    }
+    cout << "Terminating Program\n";
+    return 0;
+}
